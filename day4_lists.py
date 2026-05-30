ba9c41054948bdcbc262eb9581eaec32f9420b07
nayakdebabrata0001-dotcom <nayakdebabrata0001@gmail.com>
2026-05-30 07:07:32 +0530

    Day 4: Python Lists, Indexing, Append and For Loop

---
+++ day4_lists.py
@@ -0,0 +1,9 @@
+friends = ["Rahul", "Priya", "Amit"]
+print(friends) # ['Rahul', 'Priya', 'Amit']
+print(friends[0]) # Rahul
+print(friends[2]) # Amit
+friends.append("Sita")
+print(friends)
+print("mera sab friends")
+for dost in friends: 
+    print(dost)
