190455e7e892a3c2e4a4c8e25df3d0e7712233a3
nayakdebabrata0001-dotcom <nayakdebabrata0001@gmail.com>
2026-06-02 12:03:05 +0530

    fix: remove comma typo in print("Grade D")

---
+++ day9_ifelse.py
@@ -0,0 +1,17 @@
+marks = int(input("Enter your mark:"))
+
+if marks >=90:
+    print("Grade A")
+elif marks >=75:
+    print("Grade B")
+elif marks >=60:
+    print("Grade C")
+else:
+    print("Grade D")
+    
+num = int(input("Enter a number: "))
+
+if num %2 == 0:
+    print("Even")
+else:
+    print("odd")
